97d29a4076ce20d94d6266f7da414058eed0651d
Antonio7s <antoniolprogramador@gmail.com>
2025-02-01 15:33:27 -0300

    atualização dos models

--- app/Models/Medico.php
+++ app/Models/Medico.php
@@ -6,5 +6,27 @@ use Illuminate\Database\Eloquent\Model;
 
 class Medico extends Model
 {
-    //
+    // Relacionamento um para muitos com Agenda
+    public function agendas()
+    {
+        return $this->hasMany(Agenda::class);
+    }
+
+    // Relacionamento muitos para muitos com MedicoProcedimento
+    public function procedimentos()
+    {
+        return $this->belongsToMany(Procedimento::class, 'medico_procedimento');
+    }
+
+    // Relacionamento muitos para muitos com Especialidade
+    public function especialidades()
+    {
+        return $this->belongsToMany(Especialidade::class, 'medico_especialidade');
+    }
+
+    // Relacionamento um para muitos com Clinica
+    public function clinicas()
+    {
+        return $this->hasMany(Clinica::class);
+    }
 }
