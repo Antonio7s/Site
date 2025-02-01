56394988b0ae5a3f788ff85e4dad32cb559f1108
Antonio7s <antoniolprogramador@gmail.com>
2025-02-01 14:29:16 -0300

    fiz as migrações e os models, as tabelas estão todas feitas e construidas

---
+++ database/migrations/2025_02_01_124125_create_medicos_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('medicos', function (Blueprint $table) {
+            $table->id();  
+            $table->string('clinica'); 
+            $table->string('nome'); 
+            $table->string('endereco'); 
+            $table->string('email'); 
+            $table->string('crm'); 
+            $table->timestamps(); 
+        });
+
+    }
+    
+    public function down(): void
+    {
+        Schema::dropIfExists('medicos');
+    }
+};
