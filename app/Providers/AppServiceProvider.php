1425fed12404949f633b71b3de8f763232f3844d
Antonio7s <antoniolprogramador@gmail.com>
2025-02-08 08:34:42 -0300

    Profile pronto

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
-
 namespace App\Providers;
+
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Gate;
@@ -21,13 +21,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Defina o Gate 'access'
         Gate::define('access', function (User $user) {
-            return $user->access_level == 'admin';
+            return $user->access_level === 'admin'; // Verifica se o access_level é 'admin'
         });
 
         // Registre o componente de layout app2
         Blade::component('layouts.app2', 'app2-layout');
-
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,93 +1,92 @@
 <?php
+
+use App\Http\Controllers\PagesController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProfileController2; // 🔹 Agora está importado corretamente
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 // INDEX
-
 Route::get('/', function () {
-    return view('/Home/index');
+    return view('Home/index');
 });
 
 Route::get('/fale-conosco', function () {
-    return view('/Home/fale-conosco');
+    return view('Home/fale-conosco');
 });
 
 Route::get('/politicas-de-privacidade', function () {
-    return view('/Home/politicas-de-privacidade');
+    return view('Home/politicas-de-privacidade');
 });
 
 Route::get('/sobre-a-medexame', function () {
-    return view('/Home/sobre-a-medexame');
+    return view('Home/sobre-a-medexame');
 });
-
 
 // Rota do usuario
 Route::get('/Pagina-usuario', function () {
-    return view('/Home/paginadousuario');
+    return view('Home/paginadousuario');
 });
 
-//ADMIN DO SITE
-
+// ADMIN DO SITE
 Route::get('/admin', function () {
-    return view('/admin/login');
+    return view('admin/login');
 });
 
 Route::get('/admin/dashboard', function () {
-    return view('/admin/sub-diretorios/dashboard/vendas');
+    return view('admin/sub-diretorios/dashboard/vendas');
 });
 
-        Route::get('/admin/clinicas1', function () {
-            return view('/admin/sub-diretorios/clinicas/clinicas');
-        });
+Route::get('/admin/clinicas1', function () {
+    return view('admin/sub-diretorios/clinicas/clinicas');
+});
 
-        Route::get('/admin/clinicas2', function () {
-            return view('/admin/sub-diretorios/clinicas/registro-de-clinica');
-        });
+Route::get('/admin/clinicas2', function () {
+    return view('admin/sub-diretorios/clinicas/registro-de-clinica');
+});
 
-        Route::get('/admin/clinicas3', function () {
-            return view('/admin/sub-diretorios/clinicas/solicitacoes-de-cadastro');
-        });
+Route::get('/admin/clinicas3', function () {
+    return view('admin/sub-diretorios/clinicas/solicitacoes-de-cadastro');
+});
 
-        Route::get('/admin/clinicas4', function () {
-            return view('/admin/sub-diretorios/clinicas/analise');
-        });
+Route::get('/admin/clinicas4', function () {
+    return view('admin/sub-diretorios/clinicas/analise');
+});
 
 Route::get('/admin/usuarios', function () {
-    return view('/admin/sub-diretorios/usuarios');
+    return view('admin/sub-diretorios/usuarios');
 });
 
 Route::get('/admin/especialidades', function () {
-    return view('/admin/sub-diretorios/especialidades');
+    return view('admin/sub-diretorios/especialidades');
 });
 
-    Route::get('/admin/especialidades2', function () {
-        return view('/admin/sub-diretorios/especialidades-add');
-    });
+Route::get('/admin/especialidades2', function () {
+    return view('admin/sub-diretorios/especialidades-add');
+});
 
 Route::get('/admin/classes', function () {
-    return view('/admin/sub-diretorios/classes');
+    return view('admin/sub-diretorios/classes');
 });
 
-    Route::get('/admin/classes2', function () {
-        return view('/admin/sub-diretorios/classes-add');
-    });
-
+Route::get('/admin/classes2', function () {
+    return view('admin/sub-diretorios/classes-add');
+});
 
 Route::get('/admin/procedimentos', function () {
-    return view('/admin/sub-diretorios/procedimentos');
+    return view('admin/sub-diretorios/procedimentos');
 });
 
-    Route::get('/admin/procedimentos2', function () {
-        return view('/admin/sub-diretorios/procedimentos-add');
-    });
-
+Route::get('/admin/procedimentos2', function () {
+    return view('admin/sub-diretorios/procedimentos-add');
+});
 
 Route::get('/admin/relatorios', function () {
-    return view('/admin/sub-diretorios/relatorios');
+    return view('admin/sub-diretorios/relatorios');
 });
 
 Route::get('/admin/contatos', function () {
-    return view('/admin/sub-diretorios/contatos');
+    return view('admin/sub-diretorios/contatos');
 });
 
 Route::get('/admin/homepage', function () {
@@ -95,44 +94,41 @@ Route::get('/admin/homepage', function () {
 });
 
 Route::get('/admin/mensagens', function () {
-    return view('/admin/sub-diretorios/inbox');
+    return view('admin/sub-diretorios/inbox');
 });
 
 Route::get('/admin/lucro', function () {
-    return view('/admin/sub-diretorios/lucro');
+    return view('admin/sub-diretorios/lucro');
 });
 
 Route::get('/admin/servicos-diferenciados1', function () {
-    return view('/admin/sub-diretorios/servicos-diferenciados/visualizar');
+    return view('admin/sub-diretorios/servicos-diferenciados/visualizar');
 });
 
-    Route::get('/admin/servicos-diferenciados2', function () {
-        return view('/admin/sub-diretorios/servicos-diferenciados/adicionar');
-    });
+Route::get('/admin/servicos-diferenciados2', function () {
+    return view('admin/sub-diretorios/servicos-diferenciados/adicionar');
+});
 
 // ADMIN DA CLINICA
 Route::get('/admin-clinica', function () {
-    return view('/Clinica/Adminclinica');
+    return view('Clinica/Adminclinica');
 })->middleware(['auth:clinic']);
 
-//CLINICA INDEX
+// CLINICA INDEX
 Route::get('/clinica', function () {
-    return view('/Clinica/Paginaclinica');
+    return view('Clinica/Paginaclinica');
 });
-
 
 // BUSCA
 Route::get('/busca', function () {
-    return view('/busca/busca');
+    return view('busca/busca');
 });
 
 Route::get('/em-construcao', function () {
     return view('em-construcao');
 });
 
-
 ////////////////////////////////////////////////////
-
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -142,23 +138,36 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Grupo de rotas para ADMIN
+Route::middleware('can:access')->prefix('admin')->group(function () {
+    // Dashboard
+    Route::get('/dashboard/vendas', [PagesController::class, 'vendas'])->name('admin.dashboard.vendas');
+    Route::get('/dashboard/site', [PagesController::class, 'site'])->name('admin.dashboard.site');
+
+    // Serviços diferenciados
+    Route::prefix('dashboard/servicos-diferenciados')->group(function () {
+        Route::get('/agenda-online', [PagesController::class, 'agendaOnline'])->name('admin.dashboard.servicos.agenda-online');
+        Route::get('/classes', [PagesController::class, 'classes'])->name('admin.dashboard.servicos.classes');
+        Route::get('/contatos', [PagesController::class, 'contatos'])->name('admin.dashboard.servicos.contatos');
+        Route::get('/usuarios', [PagesController::class, 'usuarios'])->name('admin.dashboard.servicos.usuarios');
+    });
 });
 
-require __DIR__.'/auth.php';
+    });
 
+require __DIR__ . '/auth.php';
 
 ////////////////////////////////////////////2
-use App\Http\Controllers\ProfileController2;
-
 
 Route::get('/dashboard2', function () {
     return view('dashboard2');
 })->middleware(['auth:clinic', 'verified'])->name('dashboard2');
 
 Route::middleware('auth:clinic')->group(function () {
-    Route::get('/profile2', [ProfileController2::class, 'edit'])->name('profile.edit2');
-    Route::patch('/profile2', [ProfileController2::class, 'update'])->name('profile.update2');
-    Route::delete('/profile2', [ProfileController2::class, 'destroy'])->name('profile.destroy2');
+    Route::get('/profile2', [ProfileController::class, 'edit'])->name('profile.edit2');
+    Route::patch('/profile2', [ProfileController::class, 'update'])->name('profile.update2');
+    Route::delete('/profile2', [ProfileController::class, 'destroy'])->name('profile.destroy2');
 });
 
-require __DIR__.'/auth2.php';
+require __DIR__ . '/auth2.php';
