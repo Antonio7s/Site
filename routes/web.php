a5e984b1a2d69f0f8f04568ac4930ae4cd35a106
vinicius-DEV1 <vinicius.cl.dev@gmail.com>
2025-02-11 16:55:07 -0300

    organização das rotas

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\ProfileController2;
 use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
+// Importação de Controller's
 use App\Http\Controllers\Admin\ClinicaController;
 use App\Http\Controllers\Admin\UsuarioController;
 use App\Http\Controllers\Admin\EspecialidadeController;
@@ -13,54 +14,74 @@ use App\Http\Controllers\Admin\ClasseController;
 use App\Http\Controllers\Admin\ProcedimentoController;
 use App\Http\Controllers\Admin\ServicoDiferenciadoController;
 
+// Inclusão das rotas de autenticação
+require __DIR__ . '/auth.php';
+require __DIR__ . '/auth2.php';
 
-Route::prefix('admin')->group(function(){
-    Route::get('/clinicas', [ClinicaController::class, 'index'])->name('clinicas.index');
-    Route::get('/clinicas/{id}/edit', [ClinicaController::class, 'edit'])->name('clinicas.edit');
-    Route::get('/clinicas/{id}/show', [ClinicaController::class, 'show'])->name('clinicas.show');
-    Route::get('/clinicas/destroy', [ClinicaController::class, 'destroy'])->name('clinicas.destroy');
-
-    //clinicas/registro
-    //clinicas/solicitacoes-de-cadastro
-    //clinicas/solicitacoes-de-cadastro-analise
-});
 
     // Rotas que exigem autenticação de user e têm o prefixo 'admin'
 Route::middleware('auth', 'verified')->prefix('admin')->group(function () {
     Route::view('/dashboard', 'admin/sub-diretorios/dashboard/vendas')->name('dashboard.admin');
     Route::view('', 'admin/login');
+    
+    // Clínicas
+    Route::get('/clinicas', [ClinicaController::class, 'index'])->name('clinicas.index');
+    Route::get('/clinicas/{id}/edit', [ClinicaController::class, 'edit'])->name('clinicas.edit');
+    Route::get('/clinicas/{id}/show', [ClinicaController::class, 'show'])->name('clinicas.show');
+    Route::get('/clinicas/destroy', [ClinicaController::class, 'destroy'])->name('clinicas.destroy');
+    // ...
     Route::view('clinicas1', 'admin/sub-diretorios/clinicas/clinicas');
     Route::view('clinicas2', 'admin/sub-diretorios/clinicas/registro-de-clinica');
     Route::view('clinicas3', 'admin/sub-diretorios/clinicas/solicitacoes-de-cadastro');
     Route::view('clinicas4', 'admin/sub-diretorios/clinicas/analise');
+    
+    // Usuários
     Route::get('usuarios', [UsuarioController::class, 'index'])->name('usuarios.index');
-    Route::get('especialidades', [EspecialidadeController::class, 'index'])->name('especialidades.index');
-    Route::get('especialidades/create', [EspecialidadeController::class, 'create'])->name('especialidades.create');
-    Route::post('especialidades', [EspecialidadeController::class, 'store'])->name('especialidades.store');
-    Route::get('classes', [ClasseController::class, 'index'])->name('classes.index');
-    Route::get('classes', [ClasseController::class, 'create'])->name('classes.index');
-    Route::get('classes', [ClasseController::class, 'edit'])->name('classes.index');
-    Route::get('classes', [ClasseController::class, 'delet'])->name('classes.index');
-    Route::get('classes', [ClasseController::class, 'update'])->name('classes.index');
+    
+    // Especialidades
+    Route::controller(EspecialidadeController::class)->prefix('especialidades')->group(function () {
+        Route::get('especialidades', 'index')->name('especialidades.index');
+        Route::get('especialidades/create',  'create')->name('especialidades.create');
+        Route::post('especialidades', 'store')->name('especialidades.store');
+    });
 
+   // Classes
+   Route::controller(ClasseController::class)->prefix('classes')->group(function () {
+        Route::get('/', 'index')->name('classes.index');
+        Route::get('/create', 'create')->name('classes.index');
+        Route::get('/edit', 'edit')->name('classes.index');
+        Route::get('/delet', 'delet')->name('classes.index');
+        Route::get('/update', 'update')->name('classes.index');
+   });
     // Procedimentos
     Route::view('procedimentos', 'admin/sub-diretorios/procedimentos/procedimentos');
     Route::view('procedimentos2', 'admin/sub-diretorios/procedimentos/create');
+
+    // Serviços Diferenciados
+    Route::controller(ServicoDiferenciadoController::class)->prefix('servicos-diferenciados')->group(function () {
+        Route::get('/', 'index')->name('servicos-diferenciados.index');
+        Route::get('/create', 'create')->name('servicos-diferenciados.create');
+    });
+
+    // Outros
     Route::view('relatorios', 'admin/sub-diretorios/relatorios');
     Route::view('contatos', 'admin/sub-diretorios/contatos');
     Route::view('homepage', 'admin/sub-diretorios/homepage');
     Route::view('mensagens', 'admin/sub-diretorios/inbox');
-    Route::view('lucro', 'admin/sub-diretorios/lucro');
-    Route::get('servicos-diferenciados', [ServicoDiferenciadoController::class, 'index'])->name('servicos-diferenciados.index');
-    Route::get('servicos-diferenciados/create', [ServicoDiferenciadoController::class, 'create'])->name('servicos-diferenciados.create');
 });
 
-// INDEX
-Route::view('/', 'Home/index')->name('index');;
-Route::view('/fale-conosco', 'Home/fale-conosco');
-Route::view('/politicas-de-privacidade', 'Home/politicas-de-privacidade');
-Route::view('/sobre-a-medexame', 'Home/sobre-a-medexame');
-Route::view('/Pagina-usuario', 'Home/paginadousuario');
+// Páginas públicas (Index e outras estáticas)
+Route::name('public.')->group(function () {
+    Route::view('/', 'Home/index')->name('index');
+    Route::view('fale-conosco', 'Home/fale-conosco')->name('fale-conosco');
+    Route::view('politicas-de-privacidade', 'Home/politicas-de-privacidade')->name('politicas');
+    Route::view('sobre-a-medexame', 'Home/sobre-a-medexame')->name('sobre');
+    Route::view('pagina-usuario', 'Home/paginadousuario')->name('pagina-usuario');
+    // BUSCA
+    Route::view('/busca', 'busca/busca');
+    Route::view('/em-construcao', 'em-construcao');
+});
+
 
 
 // ADMIN DA CLINICA
@@ -68,13 +89,6 @@ Route::get('/admin-clinica', function () {
     return view('Clinica/Adminclinica');
 })->middleware(['auth:clinic', 'verified'])->name('admin-clinica');
 
-
-// CLINICA INDEX
-Route::view('/clinica', 'Clinica/Paginaclinica');
-
-// BUSCA
-Route::view('/busca', 'busca/busca');
-Route::view('/em-construcao', 'em-construcao');
 
 // Protegendo o dashboard do usuário com autenticação e verificação de e-mail
 Route::get('/dashboard', function () {
@@ -108,7 +122,5 @@ Route::middleware('auth:clinic')->group(function () {
     Route::delete('/profile2', [ProfileController::class, 'destroy'])->name('profile.destroy2');
 });
 
-// Inclusão das rotas de autenticação
-require __DIR__ . '/auth.php';
-require __DIR__ . '/auth2.php';
+
 
