a2168d23031b1841ff1d3abc05f5c2a5d8c3f520
vinicius-DEV1 <vinicius.cl.dev@gmail.com>
2025-02-12 15:27:16 -0300

    correcao na ordem de criacao das migration

--- database/migrations/2025_02_04_123312_create_horariodisponivel_table.php
+++ database/migrations/2025_02_04_123312_create_horariodisponivel_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('horariodisponivel', function (Blueprint $table) {
+        Schema::create('horarios_disponiveis', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
