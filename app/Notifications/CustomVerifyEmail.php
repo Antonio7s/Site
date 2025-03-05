7ea0aea2fad4db5c3cf94261b6939e7adbf1b654
vinicius-DEV1 <vinicius.cl.dev@gmail.com>
2025-03-05 12:05:25 -0300

    modif no envio de emails e no register

--- app/Notifications/CustomVerifyEmail.php
+++ app/Notifications/CustomVerifyEmail.php
@@ -3,25 +3,43 @@
 namespace App\Notifications;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
-use Illuminate\Auth\Notifications\VerifyEmail as BaseVerifyEmail;
-use Illuminate\Support\Carbon;
-use Illuminate\Support\Facades\Config;
-use Illuminate\Support\Facades\URL;
 
-class CustomVerifyEmail extends BaseVerifyEmail
+class CustomVerifyEmail extends Notification
 {
     use Queueable;
 
     /**
-     * Get the mail representation of the notification.
+     * Create a new notification instance.
      */
-    public function toMail(object $notifiable): MailMessage
+    public function __construct()
     {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via($notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable): MailMessage
+    {
+        // Gera a URL de verificação do e-mail
         $verificationUrl = $this->verificationUrl($notifiable);
 
+        // Retorna a estrutura do e-mail
         return (new MailMessage)
             ->subject('🚀 Confirme seu e-mail na MedExame!')
             ->greeting('Olá, seja bem-vindo(a)! 👋')
@@ -32,17 +50,27 @@ class CustomVerifyEmail extends BaseVerifyEmail
     }
 
     /**
-     * Generate the verification URL.
+     * Get the verification URL for the user.
+     *
+     * @param  mixed  $notifiable
+     * @return string
      */
     protected function verificationUrl($notifiable)
     {
-        return URL::temporarySignedRoute(
-            'verification.verify',
-            Carbon::now()->addMinutes(Config::get('auth.verification.expire', 60)),
-            [
-                'id' => $notifiable->getKey(),
-                'hash' => sha1($notifiable->getEmailForVerification()),
-            ]
-        );
+        // Gerar a URL de verificação do usuário
+        return $notifiable->verificationUrl();
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array<string, mixed>
+     */
+    public function toArray($notifiable): array
+    {
+        return [
+            //
+        ];
     }
 }
